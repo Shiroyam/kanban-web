bf5705c1f6a1a0d44902fc3c3a3f62099a576df4
Shiroyam-token <semenmaslina@gmail.com>
2022-10-20 19:16:11 +0300

    feat(checkbox):add ui for checkbox

---
+++ src/shared/ui/checkbox/index.tsx
@@ -0,0 +1,20 @@
+import { DetailedHTMLProps, InputHTMLAttributes } from "react"
+import styles from "./styles.module.scss"
+
+type CheckboxProps = DetailedHTMLProps<
+  InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>
+
+export const Checkbox = ({ checked, ...props }: CheckboxProps) => {
+  return (
+    <label className={styles.label}>
+      <input
+        checked={checked}
+        className={styles.checkbox}
+        type={"checkbox"}
+        {...props}
+      />
+    </label>
+  )
+}
