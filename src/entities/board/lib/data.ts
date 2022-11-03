84e34531883b12090bb3835568b0cabe984aaa1e
Shiroyam-token <semenmaslina@gmail.com>
2022-11-03 13:52:51 +0300

    feat(board): add data

---
+++ src/entities/board/lib/data.ts
@@ -0,0 +1,19 @@
+interface IBoard {
+  id: number
+  name: string
+}
+
+export const data: IBoard[] = [
+  {
+    id: 1,
+    name: "Platform Launch",
+  },
+  {
+    id: 2,
+    name: "Marketing Plan",
+  },
+  {
+    id: 3,
+    name: "Roadmap",
+  },
+]
