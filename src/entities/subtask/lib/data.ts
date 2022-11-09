829a7d21ee6ab9231a49590b0ab78ea7088ceb2f
Shiroyam-token <semenmaslina@gmail.com>
2022-11-09 19:20:43 +0300

    feat(subtask): add data

---
+++ src/entities/subtask/lib/data.ts
@@ -0,0 +1,141 @@
+interface ISubtaskData {
+  id: number
+  taskId: number
+  name: string
+  active: boolean
+}
+
+export const data: ISubtaskData[] = [
+  {
+    id: 1,
+    taskId: 1,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 2,
+    taskId: 1,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 3,
+    taskId: 1,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 4,
+    taskId: 2,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 5,
+    taskId: 2,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 6,
+    taskId: 3,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 7,
+    taskId: 3,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 8,
+    taskId: 4,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 9,
+    taskId: 4,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 10,
+    taskId: 4,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 11,
+    taskId: 5,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 12,
+    taskId: 5,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 13,
+    taskId: 6,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 14,
+    taskId: 7,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 15,
+    taskId: 7,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 16,
+    taskId: 8,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 17,
+    taskId: 8,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 18,
+    taskId: 8,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 19,
+    taskId: 9,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 20,
+    taskId: 9,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 21,
+    taskId: 9,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+  {
+    id: 22,
+    taskId: 9,
+    name: "Talk to potential customers about our proposode solution and ask for fair price expectancy",
+    active: true,
+  },
+]
